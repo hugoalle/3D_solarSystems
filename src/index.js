84e234506cd2367defe147a68465484d3a450a2a
hugoalle <hugoallemand41@hotmail.com>
2022-05-22 17:03:39 +0200

    first save

---
+++ src/index.js
@@ -0,0 +1,179 @@
+import { 
+    Scene, 
+    PerspectiveCamera, 
+    WebGLRenderer, 
+    PointLight, 
+    AxesHelper, 
+    AmbientLight, 
+    Object3D,
+    TextureLoader,
+    PMREMGenerator,
+    EquirectangularReflectionMapping,
+    sRGBEncoding,
+    ACESFilmicToneMapping,
+    LoadingManager
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+import Planete from './objets/Planete.js'
+
+//Set up : scene, camera, renderer, controls
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+
+const renderer = new WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+const controls = new OrbitControls( camera, renderer.domElement );
+
+// Style and add renderer canva
+renderer.domElement.style.width = "100%";
+renderer.domElement.style.height = "100%";
+let view = document.getElementById("view");
+view.appendChild( renderer.domElement );
+
+//Light
+const light = new PointLight( 0xffffff, 10);
+const Amblight = new AmbientLight( 0xff0000, 1);
+scene.add( light );
+scene.add( Amblight );
+
+//3D object
+const soleil = new Planete(
+{
+    color: 0xffffff,
+    emissive: 0xffffff,
+    emissiveIntensity: 1,
+    radius: 500,
+    mass: 30000,
+    moveAble: false
+});
+
+const planete1 = new Planete({
+    color: 0x200D75, 
+    metalness: 0.9, 
+    roughness: 0.5,
+    radius: 10,
+    mass:100
+});
+
+const planete2 = new Planete({
+    color: 0xF5670A, 
+    metalness: 0.9, 
+    roughness: 0.5,
+    radius: 50,
+    mass:1000,
+});
+
+const planete3 = new Planete({
+    color: 0x096C76, 
+    metalness: 0.9, 
+    roughness: 0.5,
+    radius: 200,
+    mass:5000,
+});
+
+const listOfPlanetes = [soleil, planete2, planete1, planete3]; 
+
+const solarSystem = new Object3D();
+scene.add(solarSystem);
+
+planete2.translateX(2150);
+planete2.setSpeedToOrbitAround(soleil, 'z');
+planete1.translateX(2000);
+planete1.setSpeedToOrbitAround(planete2, 'z');
+planete3.translateX(3000);
+planete3.translateZ(3000);
+planete3.setSpeedToOrbitAround(soleil);
+
+//Add planetes to solar systemes
+for (const planete of listOfPlanetes) {
+    solarSystem.add(planete.getMesh());
+}
+
+//AxeHelper
+const axes = new AxesHelper(100);
+axes.material.depthTest = false;
+axes.renderOrder = 1;
+solarSystem.add(axes);
+
+//Loading Manager
+const manager = new LoadingManager();
+manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
+
+	console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+
+};
+
+manager.onLoad = function ( ) {
+
+	console.log( 'Loading complete!');
+
+};
+// manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
+
+// 	console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+
+// };
+manager.onError = function ( url ) {
+
+	console.log( 'There was an error loading ' + url );
+
+};
+
+//IBL, background image
+renderer.outputEncoding = sRGBEncoding;
+renderer.toneMapping = ACESFilmicToneMapping;
+
+const loader = new RGBELoader( manager );
+// loader.load( "starsNASA.hdr", function(texture) {
+
+//     texture.mapping = EquirectangularReflectionMapping;
+//     scene.background = texture;
+//     scene.environment = texture;
+
+//     // take off loading view
+//     const loadingElem = document.getElementById('loading');
+//     loadingElem.style.display = 'none';
+// });
+const loadingElem = document.getElementById('loading');
+loadingElem.style.display = 'none';
+  
+
+// ---------------------------------------------
+
+camera.position.z = 0;
+camera.position.y = 2000;
+camera.lookAt(0,0,2000);
+renderer.render( scene, camera );
+
+function animate() {
+    
+	requestAnimationFrame( animate );
+
+    const speedAnimationMultiplicator = 10
+    for (let i = 0; i<speedAnimationMultiplicator; i++) {
+        
+        updatePlanetesPosition(listOfPlanetes)
+    }
+    // updatePlanetesPosition(listOfPlanetes)
+    onWindowResize();
+    controls.update();
+    renderer.render( scene, camera);
+};
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function updatePlanetesPosition(listOfPlanetes) {
+    for (const planete of listOfPlanetes) {
+        planete.updatePosition(listOfPlanetes);
+    }
+}
+
+animate();
---
+++ src/objets/Planete.js
@@ -0,0 +1,149 @@
+import {SphereGeometry, Mesh, MeshStandardMaterial, Vector3} from 'three'
+
+export default class Planete {
+
+    constructor(param) { 
+
+        this.materialValues = {
+            color: param.color || 0xffffff,
+            metalness: param.metalness || 0,
+            roughness: param.roughness || 0,
+            transparent: param.transparent || false,
+            opacity: param.opacity || 1,
+            emissive: param.emissive || 0x000000,
+            emissiveIntensity: param.emissiveIntensity || 0
+        }
+
+        this.moveAble = param.moveAble !== undefined ? param.moveAble : true;
+
+        // physical attributes
+        this.radius = param.radius || 5;
+        this.g = param.g || 10;
+        this.mass = param.mass || 50;
+        const v0 = param.v0 || [0, 0, 0];
+        this.speed = new Vector3( v0[0], v0[1], v0[2]);
+
+        // 3D display attributes
+        this.geometry = new SphereGeometry( this.radius, 32, 16);
+        this.material = new MeshStandardMaterial( this.materialValues );
+        this.mesh = new Mesh(this.geometry, this.material);
+
+        this.rate = 0.025
+    }
+
+    getMesh() {
+        return this.mesh;
+    }
+
+    translateX( tx ) {
+        this.mesh.translateX( tx );
+    }
+    translateY( ty ) {
+        this.mesh.translateY( ty );
+    }
+    translateZ( tz ) {
+        this.mesh.translateZ( tz );
+    }
+
+    translate(tx, ty, tz) {
+        this.translateX(tx);
+        this.translateY(ty);
+        this.translateZ(tz);
+    }
+
+    setSpeed(vx, vy, vz) {
+        this.speed.set(vx, vy, vz);
+    }
+
+    increaseSpeed(vx, vy, vz) {
+        this.speed.set(vx + this.speed.x, vy + this.speed.y, vz+this.speed.z);
+    }
+
+    getActualPosition() {
+        return new Vector3(this.mesh.position.x, this.mesh.position.y, this.mesh.position.z);
+    }
+
+    setSpeedRate( rate = 0.025) {
+        this.speedRate = 0.025;
+    }
+
+    setSpeedToOrbitAround(planete, beginAxis) {
+        const r = this.getActualPosition().distanceTo( planete.getActualPosition());
+        const G = 1000;
+        const M = planete.mass;
+        const speedNorm = Math.sqrt((G*M)/r);
+        console.log(` norme de la vitesse ${speedNorm} `);
+        switch (beginAxis) {
+            case "x":
+                this.setSpeed(speedNorm, 0, 0);
+                break;
+            case 'y':
+                this.setSpeed(0, speedNorm, 0);
+                break;
+            case 'z':
+                this.setSpeed(0, 0, speedNorm);
+            default:
+                this.setSpeed(0, 0, speedNorm);
+                break;
+        }
+        
+        //very important : 
+        //the planete in orbit around another one has also the speed of the planete it is orbiting around
+        this.increaseSpeed(planete.speed.x, planete.speed.y, planete.speed.z);
+    }
+
+    updatePosition(listOfPlanetes) {
+
+        // Calculate total force applied to the planete
+        const totalForce = new Vector3(0, 0, 0);
+        const actualPosition = this.getActualPosition()
+
+        for (const planete of listOfPlanetes) {
+           
+            const planetePositon = planete.getActualPosition();
+            const forceVector = new Vector3().subVectors(planetePositon, actualPosition);
+            const distanceBtwn = forceVector.length();
+
+            //if distanceBtwn == 0, we are currently calculating distance between the planet and itself
+            if (distanceBtwn > 0) {
+                
+                //avoid d to get close to 0 and that the force go to infinity
+                const d = (distanceBtwn < this.radius) ? this.radius : distanceBtwn;
+
+                forceVector.normalize();
+                //const normForce = planete.g*(this.mass);
+                const G = 1000;
+                const normForce = G*(planete.mass)*(this.mass)/(d**2);
+                forceVector.multiplyScalar(normForce);
+
+                totalForce.add( forceVector );
+            }
+
+        }
+
+        //a for acceleration
+        const a = new Vector3().copy(totalForce);
+        a.divideScalar(this.mass);
+        
+        // Calculate new position
+        const v = this.speed;
+
+        const rate = this.rate;
+
+        const vx_next = a.x*rate + v.x
+        const vy_next = a.y*rate + v.y
+        const vz_next = a.z*rate + v.z
+
+        // OM(X + dt) = v(x)*dt + OM(X)
+        const tx = v.x*rate
+        const ty = v.y*rate
+        const tz = v.z*rate
+        
+        //set new position and new speed
+        if (this.moveAble) {
+            this.translate(tx, ty, tz);
+            this.setSpeed(vx_next, vy_next, vz_next);
+        }
+    }
+
+}
